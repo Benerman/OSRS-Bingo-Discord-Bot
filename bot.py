2660f11acff6bdc4ab305e33df8d5ac90e8695c3
benerman <benermanproductions@gmail.com>
2025-12-02 23:44:45 -0500

    Added the description being posted in each channel within create_team_channels comand

--- bot.py
+++ bot.py
@@ -2122,6 +2122,7 @@ async def create_team_channels(interaction: discord.Interaction, team_name: str)
                 overwrites = overwrites_with_spectator
         else:
             channel_name = channel["name"]
+            description = channel["description"]
             if channel_name == "chat":
                 # TODO update this
                 # raise Exception("This is not implemented yet, update proper perms")
@@ -2137,7 +2138,6 @@ async def create_team_channels(interaction: discord.Interaction, team_name: str)
             )
         else:
             #TODO update this to be relevant, doesnt work for candyland and CNL
-            description = ""
             await create_discord_text_channel(interaction, channel_name, description, cat, overwrites)
         
         # all_channels.append(chan)
